07e324bc75485c649a5107c7d717199b37342fe8
aoll <agollivier@gmail.com>
2017-04-18 03:48:50 +0200

    commit -m commit -m

---
+++ Colleen.c
@@ -0,0 +1,10 @@
+/*
+	1
+*/
+#include<stdio.h>
+void f(){}
+int main(){
+/*
+	2
+*/
+char*a="/*%c%c1%c*/%c#include<stdio.h>%cvoid f(){}%cint main(){%c/*%c%c2%c*/%cchar*a=%c%s%c;printf(a,10,9,10,10,10,10,10,10,9,10,10,34,a,34);}";printf(a,10,9,10,10,10,10,10,10,9,10,10,34,a,34);}

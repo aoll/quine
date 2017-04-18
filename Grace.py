63f406f93973221a93ecf52d95e1d6a0316220c9
aoll <agollivier@gmail.com>
2017-04-18 19:20:56 +0200

    yes

---
+++ Grace.py
@@ -0,0 +1,4 @@
+FT = open("Grace_kid.py", "w")
+A = "FT = open(%cGrace_kid.py%c, %cw%c)%cA = %c%s%c%cF = FT.write(A%c(34,34,34,34,10,34,A,34,10,37,10))%cF"
+F = FT.write(A%(34,34,34,34,10,34,A,34,10,37,10))
+F

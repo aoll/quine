f38e118e09ba99554b59203a9dcaf758414e4a27
aoll <agollivier@gmail.com>
2017-04-18 19:27:17 +0200

    clean

--- Grace.py
+++ Grace.py
@@ -1,4 +1,7 @@
+"""
+	1
+"""
 FT = open("Grace_kid.py", "w")
-A = "FT = open(%cGrace_kid.py%c, %cw%c)%cA = %c%s%c%cF = FT.write(A%c(34,34,34,34,10,34,A,34,10,37,10))%cF"
-F = FT.write(A%(34,34,34,34,10,34,A,34,10,37,10))
+A = "%c%c%c%c%c1%c%c%c%c%cFT = open(%cGrace_kid.py%c, %cw%c)%cA = %c%s%c%cF = FT.write(A%c(34,34,34,10,9,10,34,34,34,10,34,34,34,34,10,34,A,34,10,37,10))%cF"
+F = FT.write(A%(34,34,34,10,9,10,34,34,34,10,34,34,34,34,10,34,A,34,10,37,10))
 F

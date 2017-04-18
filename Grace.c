42c0740abea612cd81db32d6b2afaa857f619c31
aoll <agollivier@gmail.com>
2017-04-18 18:21:16 +0200

    grace

---
+++ Grace.c
@@ -0,0 +1,8 @@
+/*
+	1
+*/
+#include <stdio.h>
+#define F FT()
+#define FT()int main(){fprintf(fopen("Grace_kid.c", "w"),A,10,9,10,10,10,10,34,34,34,34,10,34,A,34,10);}
+#define A "/*%c%c1%c*/%c#include <stdio.h>%c#define F FT()%c#define FT()int main(){fprintf(fopen(%cGrace_kid.c%c, %cw%c),A,10,9,10,10,10,10,34,34,34,34,10,34,A,34,10);}%c#define A %c%s%c%cF"
+F

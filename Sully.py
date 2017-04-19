b88141b3bd4b6cce62eb6b4dcae5173a6cefaf28
aoll <agollivier@gmail.com>
2017-04-19 13:13:46 +0200

    end

---
+++ Sully.py
@@ -0,0 +1,3 @@
+import os
+x=5;s="Sully_"+str(x-1)+".py";a='import os%cx=%d;s="Sully_"+str(x-1)+".py";a=%c%s%c%cif (x):FT=open(s,"w");FT.write(a%c(10,x-1,39,a,39,10,37));FT.close();os.system("python "+s)'
+if (x):FT=open(s,"w");FT.write(a%(10,x-1,39,a,39,10,37));FT.close();os.system("python "+s)

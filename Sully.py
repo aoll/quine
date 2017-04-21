db781b6aff70b937f10eb8277299a91abe2927e8
aoll <agollivier@gmail.com>
2017-04-21 09:21:42 +0200

    bonus Sully

--- Sully.py
+++ Sully.py
@@ -1,3 +1,7 @@
 import os
-x=5;s="Sully_"+str(x-1)+".py";a='import os%cx=%d;s="Sully_"+str(x-1)+".py";a=%c%s%c%cif (x):FT=open(s,"w");FT.write(a%c(10,x-1,39,a,39,10,37));FT.close();os.system("python "+s)'
-if (x):FT=open(s,"w");FT.write(a%(10,x-1,39,a,39,10,37));FT.close();os.system("python "+s)
+import sys
+x=5
+i=0
+if(len(sys.argv[0])>8 and sys.argv[0][5] == "_"):i=1
+s="Sully_"+str(x-i)+".py";a='import os%cimport sys%cx=%d%ci=%d%cif(len(sys.argv[0])>8 and sys.argv[0][5] == "_"):i=1%cs="Sully_"+str(x-i)+".py";a=%c%s%c%cif (x):FT=open(s,"w");FT.write(a%c(10,10,x-i,10,0,10,10,39,a,39,10,37));FT.close();os.system("python "+s)'
+if (x):FT=open(s,"w");FT.write(a%(10,10,x-i,10,0,10,10,39,a,39,10,37));FT.close();os.system("python "+s)

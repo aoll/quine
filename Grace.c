65d0a689d18ab57acc5b99164f644f275972dabf
aoll <agollivier@gmail.com>
2017-04-21 12:24:05 +0200

    modif proto

--- Grace.c
+++ Grace.c
@@ -1,8 +1,7 @@
 /*
 	1
 */
-#include <stdio.h>
-#define F FT()
-#define FT()int main(){fprintf(fopen("Grace_kid.c", "w"),A,10,9,10,10,10,10,34,34,34,34,10,34,A,34,10);}
-#define A "/*%c%c1%c*/%c#include <stdio.h>%c#define F FT()%c#define FT()int main(){fprintf(fopen(%cGrace_kid.c%c, %cw%c),A,10,9,10,10,10,10,34,34,34,34,10,34,A,34,10);}%c#define A %c%s%c%cF"
-F
+#define I "int open(const char *pathname, int flags, int mode);int dprintf(int fd, const char *format, ...);"
+#define FT() int open(const char *pathname, int flags, int mode);int dprintf(int fd, const char *format, ...); int main(){dprintf(open("Grace_kid.c",2|64|512,0777),A,10,9,10,10,34,I,34,10,I,34,34,10,34,A,34,10);}
+#define A "/*%c%c1%c*/%c#define I %c%s%c%c#define FT() %s int main(){dprintf(open(%cGrace_kid.c%c,2|64|512,0777),A,10,9,10,10,34,I,34,10,I,34,34,10,34,A,34,10);}%c#define A %c%s%c%cFT()"
+FT()
